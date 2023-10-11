bd5f7f6f7c3fb2d21d4fd4f8e04f39313276b5a5
Marcos <marcoslimabarbosa828@gmail.com>
2023-10-11 02:10:36 -0300

    re-page

--- js/main.js
+++ js/main.js
@@ -1,50 +1,89 @@
+/* #region  theme changer */
 // theme changer
 
 var toggle = document.getElementById("theme-toggle");
 
-var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-if (storedTheme)
-    document.documentElement.setAttribute('data-theme', storedTheme)
+var storedTheme = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme) document.documentElement.setAttribute("data-theme", storedTheme);
 
+toggle.onclick = function () {
+	var currentTheme = document.documentElement.getAttribute("data-theme");
+	var targetTheme = "light";
 
-toggle.onclick = function() {
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-    var targetTheme = "light";
+	if (currentTheme === "light") {
+		targetTheme = "dark";
+	}
 
-    if (currentTheme === "light") {
-        targetTheme = "dark";
-    }
-
-    document.documentElement.setAttribute('data-theme', targetTheme)
-    localStorage.setItem('theme', targetTheme);
+	document.documentElement.setAttribute("data-theme", targetTheme);
+	localStorage.setItem("theme", targetTheme);
 };
+/* #endregion */
 
+/* #region  Navbar */
 // Auto hide Navbar
 
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    var div = document.querySelector('#header'); // Substitua ".meuDiv" pelo seletor do seu div
-    
-    if (scrollPosition <= 100) {
-        div.classList.add('animaDiv');
-        div.classList.remove('animaDiv1');
-    } else {
-        div.classList.remove('animaDiv');
-        div.classList.add('animaDiv1');
-    }
+window.addEventListener("scroll", function () {
+	var scrollPosition = window.scrollY;
+	var div = document.querySelector("#header"); // Substitua ".meuDiv" pelo seletor do seu div
 
+	if (scrollPosition <= 100) {
+		div.classList.add("twhite");
+		div.classList.remove("animaDivoff");
+	} else {
+		div.classList.remove("twhite");
+		div.classList.add("animaDivoff");
+	}
 });
 
 // Responsive Navbar
 
 function toggleDiv() {
-    var div = document.getElementById("menu_bar_nav");
-    var header = document.getElementById("header");
-    if (div.style.display === "flex") {
-      div.style.display = "none";
-      header.style.borderRadius = "0 0 12px 12px"
-    } else {
-      div.style.display = "flex";
-      header.style.borderRadius = "0 0 0 0"
-    }
-  }
+	var div = document.getElementById("menu_bar_nav");
+	var header = document.getElementById("header");
+	if (div.style.display === "flex") {
+		div.style.display = "none";
+		header.classList.remove("animaDivoffb");
+	} else {
+		div.style.display = "flex";
+		header.classList.add("animaDivoffb");
+	}
+}
+/* #endregion */
+
+/* #region  Slider */
+var slides = document.getElementsByClassName("slider");
+var dots = document.getElementsByClassName("dot");
+
+var sliderIndex = 1;
+showSlides(sliderIndex);
+
+function plusSlides(n) {
+	showSlides((sliderIndex += n));
+}
+
+function currentSlides(n) {
+	showSlides((sliderIndex = n));
+}
+
+function showSlides(n) {
+	var i;
+	if (n > slides.length) {
+		sliderIndex = 1;
+	}
+	if (n < 1) {
+		sliderIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", " ");
+	}
+	slides[sliderIndex - 1].style.display = "block";
+	dots[sliderIndex - 1].className += "active";
+}
+
+setInterval(function () {
+	plusSlides(1);
+}, 6000);
+/* #endregion */
